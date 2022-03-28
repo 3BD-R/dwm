c41352b2e21934f8c3cf9932e78d73bbe24c0279
3BD-R <aminefit666@gmail.com>
2022-03-28 18:33:32 +0200

    add boxdraw

--- st-0.8.5/config.def.h
+++ st-0.8.5/config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:size=12:antialias=true:autohint=true";
-static int borderpx = 6;
+static char *font = "Liberation Mono:pixelsize=13:antialias=true:autohint=true:bold";
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -66,6 +66,20 @@ static unsigned int blinktimeout = 350;
  * thickness of underline and bar cursors
  */
 static unsigned int cursorthickness = 2;
+
+/*
+ * 1: render most of the lines/blocks characters without using the font for
+ *    perfect alignment between cells (U2500 - U259F except dashes/diagonals).
+ *    Bold affects lines thickness if boxdraw_bold is not 0. Italic is ignored.
+ * 0: disable (render all U25XX glyphs normally from the font).
+ */
+const int boxdraw = 1;
+const int boxdraw_bold = 0;
+
+/* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
+const int boxdraw_braille = 0;
+
+
 
 /*
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
@@ -171,7 +185,7 @@ static unsigned int defaultrcs = 257;
  * 7: Snowman ("☃")
  */
 // static unsigned int cursorshape = 6;
-static unsigned int cursorstyle = 1;
+static unsigned int cursorstyle = 0;
 /*
  * Default columns and rows numbers
  */
