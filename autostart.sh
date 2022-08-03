97b404543fcb54e9ce906860471f8be5a9480720
3BD-R <aminefit666@gmail.com>
2022-08-03 06:45:06 +0200

    not needed

--- autostart.sh
+++ autostart.sh
@@ -2,7 +2,6 @@
 
 #setxkbmap -layout "us,ara" -option "grp:win_space_toggle" &
 #redshift -O 3000 &
-#sh ~/.sh/fehbg &
 #/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 #changeBg &
 slstatus &
