16246ad2010d9120cda8a5832ecc97c35cedfc5f
3BD-R <aminefit666@gmail.com>
2022-08-04 23:05:45 +0200

    chngs

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -3,9 +3,9 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 3;        /* gaps between windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "SF Mono:bold:size=10", };
 static const char dmenufont[]       = "SF Mono:bold:size=10";
@@ -16,19 +16,17 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#559aff";
 static const char col_blue[]        = "#005cfb";
-static const char col_black[]       = "#000000";
-static const char col_black1[]      = "#101010";
 static const char *colors[][3]      = {
      /*               fg         bg         border   */
-       [SchemeNorm] = { col_gray3, col_black1, col_gray2 },
-       [SchemeSel]  = { col_gray4, col_blue, col_blue },
+       [SchemeNorm] = { col_gray3, col_gray0, col_gray2 },
+       [SchemeSel]  = { col_blue, col_gray0, col_blue },
 
 };
 
 static const char *const autostart[] = {
     "rfkill", "block", "bluetooth", NULL,
 	"slstatus", NULL,
-	"changBg", NULL,
+	"changeBg", NULL,
     NULL /* terminate */
 };
 
@@ -72,7 +70,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black1, "-nf", col_gray3, "-sb", col_blue, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray0, "-nf", col_gray3, "-sb", col_blue, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *lockcmd[]  = { "slock", NULL};
 
